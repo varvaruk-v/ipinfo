a32942f2812d864771157e22a8b0191d75f4223c
Вітя Варварук <viktorvarvaruk01@gmail.com>
2022-07-15 17:40:28 +0100

    v.1.0.6

--- showip/cli.py
+++ showip/cli.py
@@ -1,9 +1,10 @@
-from showip.showip import getdata
-import argparse
+def main():
+    from showip.showip import getdata
+    import argparse
 
-parser = argparse.ArgumentParser(description='Get information about your IP or any other IP')
-parser.add_argument('-ip', type=str, help='Enter IP to get information')
-args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Get information about your IP or any other IP')
+    parser.add_argument('-ip', type=str, help='Enter IP to get information')
+    args = parser.parse_args()
 
-data = getdata(args.ip)
-print(f'IP: {data["query"]}\nCountry: {data["country"]}, {data["countryCode"]}\nRegion: {data["regionName"]}, {data["region"]}\nCity: {data["city"]}\nISP: {data["isp"]}\nReverse: {data["reverse"]}')
+    data = getdata(args.ip)
+    print(f'IP: {data["query"]}\nCountry: {data["country"]}, {data["countryCode"]}\nRegion: {data["regionName"]}, {data["region"]}\nCity: {data["city"]}\nISP: {data["isp"]}\nReverse: {data["reverse"]}')
