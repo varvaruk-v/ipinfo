eef379c352fb8b8169fdc92aee73f1a75ec176e9
Вітя Варварук <viktorvarvaruk01@gmail.com>
2022-07-15 17:15:45 +0100

    v.1.0.3

--- showip/cli.py
+++ showip/cli.py
@@ -5,6 +5,5 @@ parser = argparse.ArgumentParser(description='Get information about your IP or a
 parser.add_argument('-ip', type=str, help='Enter IP to get information')
 args = parser.parse_args()
 
-showip = showip.showip()
 data = showip.get(args.ip)
 print(f'IP: {data["query"]}\nCountry: {data["country"]}, {data["countryCode"]}\nRegion: {data["regionName"]}, {data["region"]}\nCity: {data["city"]}\nISP: {data["isp"]}\nReverse: {data["reverse"]}')
--- showip/showip.py
+++ showip/showip.py
@@ -1,25 +1,24 @@
-class showip:
-    def get(self, ip=""):
-        import requests, sys
-        if ip == None or ip == "":
-            ip = ""
-        url = f"http://ip-api.com/json/{ip}?fields=66846719"
-        r = requests.get(url)
-        if r.status_code == 200:
-            data = r.json()
-            if data["status"] == "success":
-                isp = data["as"].split(" ")
-                isp.pop(0)
-                isp = " ".join(isp)
-                data["isp"] = isp
-                if data["reverse"] == "":
-                    data["reverse"] = data["query"]
-            else:
-                print(f"[FAIL] {data['message']}")
-                sys.exit()
-            return data
+def get(ip=""):
+    import requests, sys
+    if ip == None or ip == "":
+        ip = ""
+    url = f"http://ip-api.com/json/{ip}?fields=66846719"
+    r = requests.get(url)
+    if r.status_code == 200:
+        data = r.json()
+        if data["status"] == "success":
+            isp = data["as"].split(" ")
+            isp.pop(0)
+            isp = " ".join(isp)
+            data["isp"] = isp
+            if data["reverse"] == "":
+                data["reverse"] = data["query"]
         else:
-            print("[ERROR] Unknown error. Check your internet connection")
+            print(f"[FAIL] {data['message']}")
             sys.exit()
+        return data
+    else:
+        print("[ERROR] Unknown error. Check your internet connection")
+        sys.exit()
 
         
